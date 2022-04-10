a6fdb0f7c148f40a6d0d4d72f45d2f0e1efee3c0
Jingyu Zhuang <cetacean1999@gmail.com>
2022-04-10 15:56:36 -0400

    Update

--- Networking Demo/Assets/LobbyController.cs
+++ Networking Demo/Assets/LobbyController.cs
@@ -25,6 +25,78 @@ public class LobbyController : MonoBehaviour
     public PlayerObjectController LocalPlayerController;
 
     //Manager
-    private CustomNetworkManager
+    private CustomNetworkManager manager;
+    private CustomNetworkManager Manager
+    {
+        get
+        {
+            if (manager != null)
+            {
+                return manager;
+            }
+            return manager = CustomNetworkManager.singleton as CustomNetworkManager;
+        }
+    }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
 
+    public void UpdateLobbyName()
+    {
+        CurrentLobbyID = Manager.GetComponent<SteamLobby>().CurrentLobbyID;
+        LobbyNameText.text = SteamMatchmaking.GetLobbyData(new CSteamID(CurrentLobbyID), "name");
+    }
+
+    public void UpdatePlayerList()
+    {
+        if (!PlayerItemCreated)
+        {
+            CreateHostPlayerItem(); //Host
+        }
+        if (PlayerListItems.Count < Manager.GamePlayers.Count)
+        {
+            CreateClientPlayerItem();
+        }
+        if (PlayerListItems.Count > Manager.GamePlayers.Count)
+        {
+            RemovePlayerItem();
+        }
+        if (PlayerListItems.Count == Manager.GamePlayers.Count)
+        {
+            UpdatePlayerItem();
+        }
+    }
+
+    public void FindLocalPlayer()
+    {
+        LocalPlayerObject = GameObject.Find("LocalGamePlayer");
+        LocalPlayerController = LocalPlayerObject.GetComponent<PlayerObjectController>();
+    }
+
+    public void CreateHostPlayerItem()
+    {
+        foreach(PlayerObjectController player in Manager.GamePlayers)
+        {
+            GameObject NewPlayerItem = Instantiate()
+        }
+    }
+
+    public void CreateClientPlayerItem()
+    {
+
+    }
+
+    public void RemovePlayerItem()
+    {
+
+    }
+
+    public void UpdatePlayerItem()
+    {
+
+    }
 }
