8726f7dbbcef0be325de4ce9ef7f1a2de3669899
Jingyu Zhuang <cetacean1999@gmail.com>
2022-04-10 13:00:09 -0400

    init

---
+++ Networking Demo/Assets/LobbyController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Steamworks;
+using UnityEngine.UI;
+using System.Linq;
+
+public class LobbyController : MonoBehaviour
+{
+    public static LobbyController Instance;
+
+    //UI Elements
+    public TMPro.TextMeshProUGUI LobbyNameText;
+
+    //Player Data
+    public GameObject PlayerListViewContent;
+    public GameObject PlayerListItemPrefab;
+    public GameObject LocalPlayerObject;
+
+    //Other Data
+    public ulong CurrentLobbyID;
+    public bool PlayerItemCreated = false;
+    private List<PlayerListItem> PlayerListItems = new List<PlayerListItem>();
+    public PlayerObjectController LocalPlayerController;
+
+    //Manager
+    private CustomNetworkManager
+
+}
