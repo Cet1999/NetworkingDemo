b6103370cc50e851fc03998b21c89b5d3c551bcf
Jingyu Zhuang <cetacean1999@gmail.com>
2022-05-12 19:46:07 -0400

    1

---
+++ Networking Demo/Assets/CameraFollowing.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    public GameObject LocalPlayerCharacter;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
