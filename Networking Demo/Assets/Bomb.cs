54b24fc68bf711f00530dc317680ade5810d591a
Jingyu Zhuang <cetacean1999@gmail.com>
2022-05-24 21:14:59 -0400

    1

--- Networking Demo/Assets/Bomb.cs
+++ Networking Demo/Assets/Bomb.cs
@@ -6,6 +6,7 @@ using Mirror;
 public class Bomb : MonoBehaviour
 {
     public PlayerCharacterController_BombTag BombHoldingPlayerCharacterRef;
+    public float RemainingTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,18 @@ public class Bomb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RemainingTime -= Time.deltaTime;
+        if (RemainingTime <= 0)
+        {
+            Explode();
+        }
         BombHoldingPlayerCharacterRef = GameManager.instance.GetBombHoldingCharacter().ActivePlayerCharacter.GetComponent<PlayerCharacterController_BombTag>();
         transform.position += (BombHoldingPlayerCharacterRef.BombHoldingPosition.position - transform.position) / 5;
+    }
+
+    public void Explode()
+    {
+        //RemainingTime = 20;
+
     }
 }
